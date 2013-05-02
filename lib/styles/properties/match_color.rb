62238388931476c0889e3b01659aa777b0ff73c4
Aaron Royer <aaronroyer@gmail.com>
2013-05-02 07:36:54 -0400

    Allow multiple match colors

--- lib/styles/properties/match_color.rb
+++ lib/styles/properties/match_color.rb
@@ -6,11 +6,44 @@ module Styles
       strip_original_color
 
       def apply(line)
-        return line unless VALUES.include?(value)
-        # TODO: Handle situation where color codes can be matched and wreck everything - the problem
-        #       can come up when you are trying to match a number in already colorized text. Should
-        #       color be stripped from the input first by default?
-        line.gsub(selector) {|match| "#{color.send(value)}#{match}#{color.reset}" }
+        if value.is_a? Array
+          return line unless selector.is_a?(Regexp) && value.all? { |a_color| VALUES.include?(a_color) }
+          apply_colors_to_matches(line, value.dup)
+        elsif value.is_a? Symbol
+          return line unless VALUES.include?(value)
+          line.gsub(selector) { |match| "#{color.send(value)}#{match}#{color.reset}" }
+        else
+          line
+        end
+      end
+
+      private
+
+      # Use the regexp selector to determine match groups on the given line. Then apply the array
+      # of colors to the each match group, first color to first group and so on. Ignore the last
+      # match groups that do not have a corresponding color and vice versa.
+      def apply_colors_to_matches(line, colors)
+        match_data = selector.match(line)
+        return line unless match_data && (match_data.size > 1)
+        colored_line = line.dup
+
+        offsets = (1...(match_data.size)).to_a.map { |idx| match_data.offset(idx) }
+
+        # Work backward through the matches because working forward would throw off indicies.
+        # Determine the original index of the match and apply the corresponding color. If we
+        # do not have enough colors for the last match(es) then skip them.
+        # TODO: clean this up, it just feels hacky
+        offsets.reverse.each_with_index do |offset, index|
+          orig_match_index = offsets.size - index - 1
+          match_color = colors[orig_match_index]
+          next unless match_color
+
+          beg_idx, end_idx = offset
+          colored_line.insert(end_idx, color.reset)
+          colored_line.insert(beg_idx, color.send(match_color))
+        end
+        
+        colored_line
       end
     end
   end
--- test/properties/match_color_test.rb
+++ test/properties/match_color_test.rb
@@ -27,6 +27,31 @@ class MatchColorTest < MiniTest::Unit::TestCase
       ::Styles::Properties::MatchColor.new(:blue, /\d\d/).apply('the numbers 89 and 22 are in this line')
   end
 
+  def test_can_use_multiple_colors_to_match_groups
+    $snap = false
+    assert_equal "this has some color: #{color.red}this#{color.reset}",
+      ::Styles::Properties::MatchColor.new([:red], /color: (\w+)/).apply('this has some color: this')
+
+    assert_equal "numbers #{color.green}one#{color.reset} and #{color.yellow}two#{color.reset}",
+      ::Styles::Properties::MatchColor.new([:green, :yellow], /(one)[\s\w]+(two)/).apply('numbers one and two')
+
+    assert_equal "#{color.cyan}1#{color.reset} #{color.magenta}2#{color.reset} #{color.black}3#{color.reset}",
+      ::Styles::Properties::MatchColor.new([:cyan, :magenta, :black], /(\d) (\d) (\d)/).apply('1 2 3')
+  end
+
+  def test_applies_colors_correctly_with_different_numbers_of_colors_and_match_groups
+    $snap = true
+    assert_equal "numbers #{color.green}one#{color.reset} and two",
+      ::Styles::Properties::MatchColor.new([:green, :yellow], /(one)[\s\w]+two/).apply('numbers one and two')
+
+    assert_equal "numbers #{color.green}one#{color.reset} and two",
+      ::Styles::Properties::MatchColor.new([:green], /(one)[\s\w]+(two)/).apply('numbers one and two')
+
+    assert_equal "numbers #{color.green}one#{color.reset} and #{color.red}two#{color.reset} and three",
+      ::Styles::Properties::MatchColor.new([:green, :red], /(one)[\s\w]+(two)[\s\w]+three/).apply('numbers one and two and three')
+    $snap = false
+  end
+
   private
 
   def color
